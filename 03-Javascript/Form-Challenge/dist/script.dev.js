60fab6b5951c61a2fb6d01d6a67178260a75429c
Tamzin <tamzin_boyd@hotmail.co.uk>
2021-05-31 14:06:20 +0100

    JS functionality added to Form Challenge

--- 03-Javascript/Form-Challenge/dist/script.dev.js
+++ 03-Javascript/Form-Challenge/dist/script.dev.js
@@ -1,1 +1,7 @@
 "use strict";
+
+// alert when click on complete registration box
+var submitAlert = function submitAlert() {
+  alert("Thank you for registering");
+}; // if value in email box is empty, empty string,
+// change the text in paragraph to appear
---
+++ 03-Javascript/Form-Challenge/script.js
@@ -0,0 +1,7 @@
+// alert when click on complete registration box
+const submitAlert = () => {
+  alert("Thank you for registering");
+};
+
+// if value in email box is empty, empty string,
+// change the text in paragraph to appear
