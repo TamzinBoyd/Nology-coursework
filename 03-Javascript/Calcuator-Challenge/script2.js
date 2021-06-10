55909a32fb45a9bbf95bea2cea405890354651d6
Tamzin <tamzin_boyd@hotmail.co.uk>
2021-06-10 19:49:26 +0100

    basic calculator completed

--- 03-Javascript/Calcuator-Challenge/script2.js
+++ 03-Javascript/Calcuator-Challenge/script2.js
@@ -1,35 +1,84 @@
-// store values entered in input boxes
-// const box1 = document.getElementsByClassName("box1").innerHtml;
-// console.log(box1);
-// const operator = document.querySelectorAll(".operand");
-// for (let i = 0; i < operator.length; i++) {
-//   operator[i].addEventListener("click", storeValue);
-// }
-
-// function storeValue(event) {
-//   const num1 = event.target.value;
-//   console.log(num1);
-// }
-
-// user types in numbers - value stays there in input box
+// user enters numbers into input boxes
+// store box values as variables
 const box1 = document.querySelector(".box1");
 const box2 = document.querySelector(".box2");
 const box3 = document.querySelector(".box3");
 
+// user selects the desired operand
+// store the display & total as variables so they can be accessed outside of the function
+let display = document.querySelector(".display");
+let total = "";
 
-// user selects the desired operand, needs to remember this on click
-// click event to carry out function that stores value as a variable
-const operands = document.querySelectorAll(".operand");
-const total = "";
+// when users clicks on add /////////////////////////////////////////////////////////////
+// stores it as a variable then adds event listener
+const add = document.querySelector(".add");
+add.addEventListener("click", addTotal);
 
-// for loop to add event listeners to all operand buttons
-for (let i = 0; i <operands.length; i++) {
-operands[i].addEventListener("click", calculate);}
-
-// takes opperand variable then stores it's value
-function storeValue(operandAdd) {
-if (operands.value === "+") {
-    console.log("test");
+// check values in input boxes and add them together, show total in display
+function addTotal(add) {
+  // below accounts for the third box possibly being empty
+  if (box1.value & box2.value & box3.value) {
+    total =
+      parseFloat(box1.value) + parseFloat(box2.value) + parseFloat(box3.value);
+    display.innerHTML = total;
+  } else {
+    total = parseFloat(box1.value) + parseFloat(box2.value);
+    display.innerHTML = total;
+  }
 }
 
-// clicks clicks calculate and the calculator carries out the sum based on value, consider if statement
+// when users clicks on minus /////////////////////////////////////////////////////////////
+const minus = document.querySelector(".minus");
+minus.addEventListener("click", minusTotal);
+
+function minusTotal(minus) {
+  if (box1.value & box2.value & box3.value) {
+    total =
+      parseFloat(box1.value) - parseFloat(box2.value) - parseFloat(box3.value);
+    display.innerHTML = total;
+  } else {
+    total = parseFloat(box1.value) - parseFloat(box2.value);
+    display.innerHTML = total;
+  }
+}
+
+// when users clicks on divide /////////////////////////////////////////////////////////////
+const divide = document.querySelector(".divide");
+divide.addEventListener("click", divideTotal);
+
+function divideTotal(divide) {
+  if (box1.value & box2.value & box3.value) {
+    total =
+      parseFloat(box1.value) / parseFloat(box2.value) / parseFloat(box3.value);
+    display.innerHTML = total;
+  } else {
+    total = parseFloat(box1.value) / parseFloat(box2.value);
+    display.innerHTML = total;
+  }
+}
+
+// when users clicks on minus /////////////////////////////////////////////////////////////
+const multiply = document.querySelector(".multiply");
+multiply.addEventListener("click", multiplyTotal);
+
+function multiplyTotal(multiply) {
+  if (box1.value & box2.value & box3.value) {
+    total =
+      parseFloat(box1.value) * parseFloat(box2.value) * parseFloat(box3.value);
+    display.innerHTML = total;
+  } else {
+    total = parseFloat(box1.value) * parseFloat(box2.value);
+    display.innerHTML = total;
+  }
+}
+
+// // reset button to change input value
+const reset = document.querySelector(".reset");
+reset.addEventListener("click", resetCalculator);
+
+function resetCalculator(reset) {
+  box1.value = "";
+  box2.value = "";
+  box3.value = "";
+  display.innerHTML = 0;
+}
