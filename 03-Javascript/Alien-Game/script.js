88a441380bc93a0cc42ce6e00c31dd88f30ffe88
Tamzin <tamzin_boyd@hotmail.co.uk>
2021-09-13 17:21:23 +0100

    Experimented with array methods

--- 03-Javascript/Alien-Game/script.js
+++ 03-Javascript/Alien-Game/script.js
@@ -8,46 +8,48 @@
 // scores will need to accessable throughout.
 
 class Ship {
-  constructor(shipType, pointsTotal, pointsDeducted) {
+  constructor(shipType, currentPoints, pointsDeducted) {
     this.shipType = shipType;
-    this.pointsTotal = pointsTotal;
+    this.currentPoints = currentPoints;
     this.pointsDeducted = pointsDeducted;
   }
 
   deductPoints() {
-    return this.pointsTotal - this.pointsDeducted;
-  }
-
-  currentPoints() {
-    return (this.currentPoints = currentPoints);
+    return (this.currentPoints = this.currentPoints - this.pointsDeducted);
+    console.log(this.currentPoints);
   }
 }
 
-let MotherShip = new Ship("mother", 100, 100);
-let DefenseShip = new Ship("de0....................fense", 80, 10);
-let AttackShip = new Ship("Attack", 45, 12);
+let motherShip = new Ship("mother", 100, 100);
+let defenseShip = new Ship("defense", 80, 10);
+let attackShip = new Ship("attack", 45, 12);
 
-if (
-  MotherShip.deductPoints() === 0 &&
-  DefenseShip.deductPoints() === 0 &&
-  AttackShip.deductPoints() === 0
-) {
-  // return
-  gameOver();
-}
 // what happens when click shoot button
 // a random ship object is hit.
 // Check if ship has points left
-// if not - hit another ship
+// if not, check if other ships have 0 point - game over, or hit another ship
 // if so deduct the points and update current points
-// so each ship can take a different number of hits, so decide how when you click shoot it decides which
-// ship to hit, and how to know when a ship has been sunk
-// when ship is hit it activtes the function to deduct points
-// checks if points totals = 0, if so run game over function
+let shipArr = [motherShip, defenseShip, attackShip];
+let randomNum = 0;
+
 const shootButton = document.querySelector(".shoot-button");
 shootButton.addEventListener("click", function () {
-  let randomNum = Math.floor(Math.random() * 83);
-  console.log(randomNum);
+  //   check if all ships have 0 points
+  if (
+    motherShip.currentPoints === 0 &&
+    defenseShip.currentPoints === 0 &&
+    attackShip.currentPoints === 0
+  ) {
+    gameOver();
+  } else {
+    //   use random number to select element in ship array
+    randomNum = shipArr[Math.floor(Math.random() * shipArr.length)];
+  }
+  if (randomNum.shipType === "attack") {
+    console.log(randomNum.shipType);
+  } else {
+    alert("wrong ship");
+  }
 });
 
 // mother ship - can take 1 hit
--- 03-Javascript/Morse-Code-Translator/scipt.js
+++ 03-Javascript/Morse-Code-Translator/scipt.js
@@ -41,50 +41,52 @@ const englishAlphabet = {
   " ": "  ",
 };
 
-const morseAlphabet = {
-  " ": "",
-  "-": " ",
-  ".-": "a",
-  "-...": "b",
-  "-.-.": "c",
-  "-..": "d",
-  e: ".",
-  f: "..-.",
-  g: "--.",
-  h: "....",
-  i: "..",
-  j: ".---",
-  k: "-.-",
-  l: ".-..",
-  m: "--",
-  n: "-.",
-  o: "---",
-  p: ".--.",
-  q: "--.-",
-  r: ".-.",
-  s: "...",
-  t: "-",
-  u: "..-",
-  v: "...-",
-  w: ".--",
-  x: "-..-",
-  y: "-.--",
-  z: "--..",
-  1: ".----",
-  2: "..---",
-  3: "...--",
-  4: "....-",
-  5: ".....",
-  6: "-....",
-  7: "--...",
-  8: "---..",
-  9: "----.",
-  0: "-----",
-  ".": ".-.-.-",
-  ",": "--..--",
-  "?": "..--..",
-  " ": " ",
-};
+const morseAlphabet = [
+  {
+    " ": "",
+    "-": " ",
+    ".-": "a",
+    "-...": "b",
+    "-.-.": "c",
+    "-..": "d",
+    e: ".",
+    f: "..-.",
+    g: "--.",
+    h: "....",
+    i: "..",
+    j: ".---",
+    k: "-.-",
+    l: ".-..",
+    m: "--",
+    n: "-.",
+    o: "---",
+    p: ".--.",
+    q: "--.-",
+    r: ".-.",
+    s: "...",
+    t: "-",
+    u: "..-",
+    v: "...-",
+    w: ".--",
+    x: "-..-",
+    y: "-.--",
+    z: "--..",
+    1: ".----",
+    2: "..---",
+    3: "...--",
+    4: "....-",
+    5: ".....",
+    6: "-....",
+    7: "--...",
+    8: "---..",
+    9: "----.",
+    0: "-----",
+    ".": ".-.-.-",
+    ",": "--..--",
+    "?": "..--..",
+    " ": " ",
+  },
+];
 
 // translate English to Morse code //////////////////////////////////
 document
@@ -120,16 +122,20 @@ document
     let getMorseInput = document.querySelector(".entry-box-morse").value;
     // getEnglishInput = getEnglishInput.toLowercase();
     getMorseInput = getMorseInput.split(" ");
-
-    let getEnglishBox = document.querySelector(".entry-box-english");
-
-    // want to take each "character" from Morse Code input (e.g. .-)
-    // check it against the morseAlphabet object
-    // return the value that matches the key
-    // const getMorseCharacters = () => {
-    //   getMorseInput.map((morseCharacter) => {
-    //     console.log(morseCharacter);
-    //   });
-    // };
-    // getEnglishBox.value = getMorseCharacters(morseAlphabet);
+    const test = morseAlphabet.find((item) => {
+      return item.getMorseInput === ".-";
+    });
+    console.log(test);
   });
+
+let getEnglishBox = document.querySelector(".entry-box-english");
+
+// want to take each "character" from Morse Code input (e.g. .-)
+// check it against the morseAlphabet object
+// return the value that matches the key
+// const getMorseCharacters = () => {
+//   getMorseInput.map((morseCharacter) => {
+//     console.log(morseCharacter);
+//   });
+// };
+// getEnglishBox.value = getMorseCharacters(morseAlphabet);
