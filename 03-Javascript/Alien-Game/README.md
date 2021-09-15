6b7137a5e012b18e6085a14875a5e40c0ef666a3
Tamzin <tamzin_boyd@hotmail.co.uk>
2021-09-15 20:33:53 +0100

    Alien Game score system fixed

---
+++ 03-Javascript/Alien-Game/README.md
@@ -0,0 +1,14 @@
+BRIEF
+
+Create a text based alien shooting game
+3 types of ship holding different hit scores
+When a ship is hit the points are deducted
+Game over is when all ships have 0 points left
+
+FUNCTIONALITY
+
+I want to store each ship in an Object, created from a class as all keys will be the same for each ship.
+Need to be able to store current points for each ship.
+Button to generate a random number/ship allocation
+When the ship is hit, access the object to deduct points and update current points
+Recognise when all current points are 0 and show "game over"
--- 03-Javascript/Alien-Game/script.js
+++ 03-Javascript/Alien-Game/script.js
@@ -1,12 +1,3 @@
-// re-watch car object video for ideas on how functions interact
-
-// Create a text based alien shooting game
-
-// 3 types of ship - store in classes/objects
-// each ship contains a score & function for when they are hit
-// Game is over when all scores = 0
-// scores will need to accessable throughout.
-
 class Ship {
   constructor(shipType, currentPoints, pointsDeducted) {
     this.shipType = shipType;
@@ -15,23 +6,13 @@ class Ship {
   }
 
   deductPoints() {
-    if (this.currentPoints >= 0) {
-      return (this.currentPoints = this.currentPoints - this.pointsDeducted);
-    } else {
-      this.currentPoints = this.currentPoints;
-    }
+    return (this.currentPoints = this.currentPoints - this.pointsDeducted);
   }
 }
-
 let motherShip = new Ship("mother", 100, 100);
 let defenseShip = new Ship("defense", 80, 10);
 let attackShip = new Ship("attack", 48, 12);
 
-// what happens when click shoot button
-// a random ship object is hit.
-// Check if ship has points left
-// if not, check if other ships have 0 point - game over, or hit another ship
-// if so deduct the points and update current points
 let shipArr = [motherShip, defenseShip, attackShip];
 let randomNum = 0;
 
@@ -54,40 +35,17 @@ shootButton.addEventListener("click", function () {
 });
 
 function hitShip() {
-  randomNum = shipArr[Math.floor(Math.random() * shipArr.length)];
-  console.log(randomNum);
-  if (
-    randomNum.shipType === "attack" &&
-    attackShip.currentPoints > 0
-    // defenseShip.currentPoints > 0 &&
-    // motherShip.currentPoints > 0
-  ) {
-    attackShip.deductPoints();
-    attackCurrentScore.innerHTML = attackShip.currentPoints;
-  } else if (
-    randomNum.shipType === "defense" &&
-    defenseShip.currentPoints > 0
-    // attackShip.currentPoints > 0 &&
-    // motherShip.currentPoints > 0
-  ) {
-    defenseShip.deductPoints();
-    defenseCurrentScore.innerHTML = defenseShip.currentPoints;
-  } else if (
-    randomNum.shipType === "mother" &&
-    motherShip.currentPoints > 0
-    // defenseShip.currentPoints > 0 &&
-    // attackShip.currentPoints > 0
-  ) {
-    motherShip.deductPoints();
-    motherCurrentScore.innerHTML = motherShip.currentPoints;
+  randomShip = shipArr[Math.floor(Math.random() * shipArr.length)];
+  if (randomShip.currentPoints <= 0) {
+    hitShip();
   } else {
-    gameOver();
+    console.log(randomShip.currentPoints);
+    randomShip.deductPoints();
+    motherCurrentScore.innerHTML = motherShip.currentPoints;
+    defenseCurrentScore.innerHTML = defenseShip.currentPoints;
+    attackCurrentScore.innerHTML = attackShip.currentPoints;
   }
 }
-
-// mother ship - can take 1 hit
-// each defence ship (5 of) can take 8 hits (40)
-// each attack ship(8 of) can take 4 (32)
 
 function gameOver() {
   alert("Game over");
