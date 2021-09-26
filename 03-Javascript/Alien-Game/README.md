03a4918c50997e0f20ea9ba473e5b44b30caed5c
Tamzin <tamzin_boyd@hotmail.co.uk>
2021-09-26 09:33:33 +0100

    HTML interaction added to game

--- 03-Javascript/Alien-Game/README.md
+++ 03-Javascript/Alien-Game/README.md
@@ -1,14 +1,20 @@
 BRIEF
 
-Create a text based alien shooting game
-3 types of ship holding different hit scores
+Create a text based/basic alien shooting game
+3 types of ships holding different hit scores
 When a ship is hit the points are deducted
-Game over is when all ships have 0 points left
+
+Game over is when all ships have 0 points left or Mother ship has 0 points
 
 FUNCTIONALITY
 
 I want to store each ship in an Object, created from a class as all keys will be the same for each ship.
 Need to be able to store current points for each ship.
-Button to generate a random number/ship allocation
-When the ship is hit, access the object to deduct points and update current points
-Recognise when all current points are 0 and show "game over"
+Button to generate a random number/ship allocation to hit.
+When the ship is hit, access the object to deduct points and update current points.
+Recognise when all current points are 0/Mother ship 0 and show "game over - you've won".
+When a ship reaches 0 points a ship on HTML needs a hidden classlist.
+
+OTHER ADDITIONS?
+Consider time limit?
+End of game alert, consider HTML with CSS hidden class
--- 03-Javascript/Alien-Game/script.js
+++ 03-Javascript/Alien-Game/script.js
@@ -9,22 +9,49 @@ class Ship {
     return (this.currentPoints = this.currentPoints - this.pointsDeducted);
   }
 }
-let motherShip = new Ship("mother", 100, 100);
+
+// create objects for each ship
+let motherShip = new Ship("mother", 100, 9);
 let defenseShip = new Ship("defense", 80, 10);
+let defenseShip2 = new Ship("defense", 80, 10);
+let defenseShip3 = new Ship("defense", 80, 10);
+let defenseShip4 = new Ship("defense", 80, 10);
+let defenseShip5 = new Ship("defense", 80, 10);
+
 let attackShip = new Ship("attack", 48, 12);
+let attackShip2 = new Ship("attack", 48, 12);
+let attackShip3 = new Ship("attack", 48, 12);
+let attackShip4 = new Ship("attack", 48, 12);
+let attackShip5 = new Ship("attack", 48, 12);
+let attackShip6 = new Ship("attack", 48, 12);
+let attackShip7 = new Ship("attack", 48, 12);
+let attackShip8 = new Ship("attack", 48, 12);
 
-let shipArr = [motherShip, defenseShip, attackShip];
-let randomNum = 0;
-
-let motherCurrentScore = document.querySelector(".mother-score");
-let defenseCurrentScore = document.querySelector(".defense-score");
-let attackCurrentScore = document.querySelector(".attack-score");
+// put objects into an array
+let shipArr = [
+  motherShip,
+  defenseShip,
+  defenseShip2,
+  defenseShip3,
+  defenseShip4,
+  defenseShip5,
+  attackShip,
+  attackShip2,
+  attackShip3,
+  attackShip4,
+  attackShip5,
+  attackShip6,
+  attackShip7,
+  attackShip8,
+];
+let randomShip = 0;
 
 const shootButton = document.querySelector(".shoot-button");
 shootButton.addEventListener("click", function () {
   //   check if all ships have 0 points
-  if (
-    motherShip.currentPoints === 0 &&
+  if (motherShip.currentPoints === 0) {
+    gameOver();
+  } else if (
     defenseShip.currentPoints === 0 &&
     attackShip.currentPoints === 0
   ) {
@@ -34,24 +61,57 @@ shootButton.addEventListener("click", function () {
   }
 });
 
-function hitShip() {
+checkHtmlTest = () => {
+  // used attack as example to see if could delete a random ship on the screen
+  if (randomShip.currentPoints <= 0 && randomShip.shipType === "attack") {
+    // collect all ships with classlist
+    const shootShip = document.querySelectorAll(".attackship");
+    // as gives Node list, tried to push these into an array
+    const testArr = [];
+    shootShip.forEach((word) => {
+      testArr.push(word);
+      // console.log(testArr);
+    });
+
+    for (let i = 0; i < testArr.length; i++) {
+      // is the ship in current array already hidden?
+      if (testArr[i].classList.contains("hide")) {
+        // if yes do the test again to access the next ship
+        checkHtmlTest();
+        // if not add hide to current element then break out as don't want to hide more than one ship
+      } else {
+        // when reach this point it shows the console log but doesn't add a classlist
+        testArr[i].classList.add("hide");
+        console.log("reached else statement");
+        console.log(testArr[i].classList);
+        break;
+      }
+    }
+  }
+
+  // check score of current ship
+
+  // if 0 then delete ship
+};
+const hitShip = () => {
+  // choose random ship
   randomShip = shipArr[Math.floor(Math.random() * shipArr.length)];
+
   if (randomShip.currentPoints <= 0) {
     hitShip();
+    checkHtmlTest();
   } else {
     console.log(randomShip.currentPoints);
+    console.log(randomShip);
     randomShip.deductPoints();
-    motherCurrentScore.innerHTML = motherShip.currentPoints;
-    defenseCurrentScore.innerHTML = defenseShip.currentPoints;
-    attackCurrentScore.innerHTML = attackShip.currentPoints;
   }
-}
+};
 
-function gameOver() {
+const gameOver = () => {
   alert("Game over");
-  console.log(
-    motherShip.currentPoints,
-    defenseShip.currentPoints,
-    attackShip.currentPoints
-  );
-}
+};
+
+// add a hidden class list
+// need to get the ship from HTML - not sure how as need to link it to the object
+
+// create an HTML element for game over that is only visible when game over is activated - class list
