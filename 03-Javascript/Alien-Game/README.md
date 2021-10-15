b232a0b663d47557f230f89682b54595a93d32b6
Tamzin <tamzin_boyd@hotmail.co.uk>
2021-10-15 21:06:52 +0100

    reset button bug fixed

--- 03-Javascript/Alien-Game/README.md
+++ 03-Javascript/Alien-Game/README.md
@@ -4,23 +4,21 @@ Create a text based/basic alien shooting game
 3 types of ships holding different hit scores
 When a ship is hit the points are deducted
 
-Game over is when all ships have 0 points left or Mother ship has 0 points
-Aim for less than 50 lines of code
+Game over is when all ships have 0 points or Mother ship has 0 points
 
 FUNCTIONALITY
 
 I want to store each ship in an Object, created from a class as all keys will be the same for each ship.
-Need to be able to store current points for each ship.
-Button to generate a random number/ship allocation to hit.
+Method to calculate current points based on points minus hit deduct points.
+Button to generate a random ship allocation to hit.
 When the ship is hit, access the object to deduct points and update current points.
+When a ship reaches 0 points, link to HTML and add a hidden class
 Recognise when all current points are 0/Mother ship 0 and show "game over - you've won".
-When a ship reaches 0 points a ship on HTML needs a hidden classlist.
+Restart button to start the game again
+
 
 IMPROVEMENTS NEEDED
 
-- Add remaining points to HTML?
-- Bug with Grid in CSS when adapting to mobile
-- Reset button - currently it doesn't reset points or class lists due to bug
-- Improve responsiveness for grid elements
-- Refactor code
 - Carry out unit testing once functioning
+- Add timer
+- Add animation for shooting action
--- 03-Javascript/Alien-Game/script.js
+++ 03-Javascript/Alien-Game/script.js
@@ -9,40 +9,70 @@ class Ship {
   }
 }
 
-// create objects for each ship & push into an array
-let motherShip = new Ship("mother", 100, 9);
-let defenceShip = new Ship("defence", 80, 10);
-let defenceShip2 = new Ship("defence", 80, 10);
-let defenceShip3 = new Ship("defence", 80, 10);
-let defenceShip4 = new Ship("defence", 80, 10);
-let defenceShip5 = new Ship("defence", 80, 10);
-let attackShip = new Ship("attack", 48, 12);
-let attackShip2 = new Ship("attack", 48, 12);
-let attackShip3 = new Ship("attack", 48, 12);
-let attackShip4 = new Ship("attack", 48, 12);
-let attackShip5 = new Ship("attack", 48, 12);
-let attackShip6 = new Ship("attack", 48, 12);
-let attackShip7 = new Ship("attack", 48, 12);
-let attackShip8 = new Ship("attack", 48, 12);
+const element = document.querySelector(".defence");
+const gameOverMessage = document.querySelector(".game-over");
+const shootButton = document.querySelector(".shoot-button");
+const restartButton = document.querySelector(".restart-button");
 
+let messageToPlayer = document.querySelector(".message");
+let randomShip;
+let randomHtml;
+
+// create objects for each ship & push into an array
+let motherShip;
+let defenceShip;
+let defenceShip2;
+let defenceShip3;
+let defenceShip4;
+let defenceShip5;
+let attackShip;
+let attackShip2;
+let attackShip3;
+let attackShip4;
+let attackShip5;
+let attackShip6;
+let attackShip7;
+let attackShip8;
 let shipArr = [];
-shipArr.push(
-  defenceShip,
-  defenceShip2,
-  defenceShip3,
-  defenceShip4,
-  defenceShip5,
-  attackShip,
-  attackShip2,
-  attackShip3,
-  attackShip4,
-  attackShip5,
-  attackShip6,
-  attackShip7,
-  attackShip8
-);
+
+const loadGame = () => {
+  motherShip = new Ship("mother", 100, 9);
+  defenceShip = new Ship("defence", 80, 10);
+  defenceShip2 = new Ship("defence", 80, 10);
+  defenceShip3 = new Ship("defence", 80, 10);
+  defenceShip4 = new Ship("defence", 80, 10);
+  defenceShip5 = new Ship("defence", 80, 10);
+  attackShip = new Ship("attack", 48, 12);
+  attackShip2 = new Ship("attack", 48, 12);
+  attackShip3 = new Ship("attack", 48, 12);
+  attackShip4 = new Ship("attack", 48, 12);
+  attackShip5 = new Ship("attack", 48, 12);
+  attackShip6 = new Ship("attack", 48, 12);
+  attackShip7 = new Ship("attack", 48, 12);
+  attackShip8 = new Ship("attack", 48, 12);
+
+  shipArr.push(
+    motherShip,
+    defenceShip,
+    defenceShip2,
+    defenceShip3,
+    defenceShip4,
+    defenceShip5,
+    attackShip,
+    attackShip2,
+    attackShip3,
+    attackShip4,
+    attackShip5,
+    attackShip6,
+    attackShip7,
+    attackShip8
+  );
+};
+
+loadGame();
 
 // get each ship's ID from HTLML & push into array
+const motherHtml = document.getElementById("mother");
 const attackHtml1 = document.getElementById("attack1");
 const attackHtml2 = document.getElementById("attack2");
 const attackHtml3 = document.getElementById("attack3");
@@ -56,8 +86,10 @@ const defenceHtml2 = document.getElementById("defence2");
 const defenceHtml3 = document.getElementById("defence3");
 const defenceHtml4 = document.getElementById("defence4");
 const defenceHtml5 = document.getElementById("defence5");
+let htmlArr = [];
 
-let htmlArr = [
+htmlArr.push(
+  motherHtml,
   attackHtml1,
   attackHtml2,
   attackHtml3,
@@ -70,31 +102,20 @@ let htmlArr = [
   defenceHtml2,
   defenceHtml3,
   defenceHtml4,
-  defenceHtml5,
-];
+  defenceHtml5
+);
 
-// get other elements
-const element = document.querySelector(".defence");
-const gameOverMessage = document.querySelector(".game-over");
-const shootButton = document.querySelector(".shoot-button");
-const restartButton = document.querySelector(".restart-button");
-
-let messageToPlayer = document.querySelector(".message");
-let randomShip = 0;
-
-// shoot button ////////////////////////////////////////////////////////////////////////////////////
+// shoot button //////////////////////////////////////////////////////////////////////////
 const shootShip = () => {
   if (
-    (motherShip.currentPoints === 0) | (defenceShip.currentPoints === 0) &&
-    attackShip.currentPoints === 0
+    motherShip.currentPoints === 0 ||
+    (defenceShip.currentPoints === 0 && attackShip.currentPoints === 0)
   ) {
     gameOver();
   } else {
     hitShip();
   }
 };
-
-shootButton.addEventListener("click", shootShip);
 
 const hitShip = () => {
   // choose random ship from array on each hit
@@ -108,6 +129,7 @@ const hitShip = () => {
   } else {
     messageToPlayer.innerHTML = `${randomShip.shipType} ship has been hit!`;
     randomShip.deductPoints();
+    console.log(motherShip.currentPoints);
 
     if (randomShip.currentPoints <= 0) {
       hideShip();
@@ -116,60 +138,40 @@ const hitShip = () => {
   }
 };
 
-// hiding the HTML elements of the ship once sunk (currently in an array) ////////////////////////////////////////////////////////////
-hideShip = () => {
-  // select random ship from array
-  const randomHtml = htmlArr[Math.floor(Math.random() * htmlArr.length)];
+// hiding the HTML elements of the ship once sunk (currently in an array) ////////////////
+const hideShip = () => {
+  randomHtml = htmlArr[Math.floor(Math.random() * htmlArr.length)];
 
   // if ship is already sunk then choose another ship in array. Mothership N/A as 0 points means game over
   if (randomHtml.classList.contains("sunk-ship")) {
     hideShip();
-    // hide defence ship
   } else if (randomHtml.classList.contains("defenceship")) {
     randomHtml.classList.add("sunk-ship");
-
-    //  hide attacked ship
   } else if (randomHtml.classList.contains("attackship")) {
     randomHtml.classList.add("sunk-ship");
   }
 };
 
-// show game over message and hide shoot button ///////////////////////////////////////////////////////////////////////////////////
-gameOver = () => {
+// show game over message and hide shoot button //////////////////////////////////////////
+const gameOver = () => {
   gameOverMessage.classList.remove("hidden");
   shootButton.classList.add("hidden");
 };
 
-// reset button - currently doesn't work
+// restart button //////////////////////////////////////////////////////////////
+const restartClasses = () => {
+  htmlArr.forEach((element) => {
+    element.classList.remove("sunk-ship");
+  });
+};
+
 restartGame = () => {
   gameOverMessage.classList.add("hidden");
   shootButton.classList.remove("hidden");
   messageToPlayer.innerHTML = "Hit shoot to start the game";
-
-  // below doesn't work, says element isn't a function? /////////////////////////////////
-  // trying to reset the values of t
-  shipArr.forEach((element) => {
-    if (element.shipType.contains("defence")) {
-      element.currentPoints = 90;
-    } else if (element.shipType.contains("attack")) {
-      element.currentPoints = 48;
-    } else {
-      return shipArr;
-    }
-  });
-  htmlArr.forEach((ship) => {
-    ship.classList.remove("sunk-ship");
-  });
-  console.log(motherShip.currentPoints, defenceShip2.currentPoints);
+  loadGame();
+  restartClasses();
 };
 
+shootButton.addEventListener("click", shootShip);
 restartButton.addEventListener("click", restartGame);
-
-// below was adding HTML, but couldnt then add classes to it
-// loop over array to create ships in the HTML (not random)
-// for (let i = 0; i < shipArr.length; i++) {
-//   const createShips = document.createElement("h3");
-//   const textEl = document.createTextNode(shipArr[i].shipType);
-//   createShips.appendChild(textEl);
-//   element.appendChild(createShips);
-// }
