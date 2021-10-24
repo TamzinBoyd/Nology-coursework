5ea6eb04edea23962d6500f69aea5e29e5f6cc89
Tamzin <tamzin_boyd@hotmail.co.uk>
2021-10-24 21:07:30 +0100

    Hangman styling added

--- 03-Javascript/Hangman-Game/README.md
+++ 03-Javascript/Hangman-Game/README.md
@@ -1,19 +1,47 @@
 How does hangman work?
 
-A random word is chosen by the game and hidden to user
-On each turn user guesses a letter. If the letter if in the word, it's revealed
-If not points are deducted (items removed from hangman)
+A random word is chosen by player 1 (computer)
+On each turn the user guesses a letter. If the letter is in the word, it's revealed
+If not points are deducted and hangman starts to appear
 
-User wins when the guess the word / all letters
-User loses when all points deducted / man is hung.
+User wins when they guess all letters of the word
+User loses when all points deducted / hangman appears fully
 
-IDEAS
+HOW TO APPROACH
 Array of words
-Each turn a random item from the array is generated (using math.random and [for index])
-User enters letter in input box, clicks button to submit.
-Comp checks if the letter is in the string of the chosen word - filter / if statement?
-Need to show letters used - visible array
+Each game a random item from the array is generated (using math.random)
+User enters letter in an input box and clicks a button to submit.
+Computer checks if the letter is in the string of the chosen word.
+If so the letter replaces a space in the hidden word /
+if not, part of the hangman appears (using classlist)
 
-Letter is revealed on the game
-OR
-points deducted
+Main challenge seemed to be finding a way to show the user which letters they had already chosen.
+Started exploring an array, which had values pushed into it.
+Then decided to display an alphabet and remove the letters from the alphabet array each go
+
+START GAME
+
+- word is chosen
+- alphabet shown
+- score set to 10
+
+GET & CHECK A VALUE
+
+- enter value in input box & store
+- convert all to lowercase
+- check if value is in the chosen word with loop, if so update chosen word to show the value
+- if not score needs to decrease by 1 & classlist removed from hangman element
+- Alphabet to update minus the value
+
+WIN THE GAME
+
+- If chosen word doesn't include any "\_"
+- Notify the user that they have won in HTML
+
+GAME OVER
+
+- when score reachs 0, game over is activated
+- show a reset button to: reset score to 10, choose another word, reset alphabet array
+
+IDEAS TO ADD
+Difficulty levels - could determine what level the score starts at
--- 03-Javascript/Hangman-Game/script.js
+++ 03-Javascript/Hangman-Game/script.js
@@ -1,72 +1,143 @@
 // create films
-const filmsArr = ["snow white", "black beauty", "terminator", "step brothers"];
+const filmsArr = ["snow shite", "black beauty", "tertinator", "step brothers"];
 // get elements
 const getLetter = document.querySelector(".input");
 const button = document.querySelector(".button");
 const message = document.querySelector(".message");
 const hiddenWord = document.querySelector("#hiddenWord");
 const start = document.querySelector(".start");
-const usedLetters = document.querySelector(".usedLetters");
-const errorMessage = document.querySelector(".error");
-const scoreTotal = document.querySelector(".score");
+const lettersContainer = document.querySelector(".letters-container");
+const alphabet = document.querySelector(".usedLetters");
+const hangmanContainer = document.querySelector(".hangman");
+const hangman = document.querySelectorAll(".line");
+const gameOverMessage = document.querySelector(".game-over");
+const restartGame = document.querySelector(".reset");
 
 let chosenWord;
 let chosenWordArr = [];
+let alphabetArr = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
 let value;
-let usedLettersArr = [];
-let score = 5;
+let score = 10;
+let booleon;
 
+alphabet.innerHTML = alphabetArr.join(" ");
+
+// START GAME /////////////////////////////////////////////////////////////////////
 // choose a random element from array (word)
-// Change to array so can loop over it and add _ for each letter in the chosen word - returning _ _ _ etc
-const chooseWord = () => {
+// add _ to an array for each letter in the chosen word
+const startGame = () => {
   chosenWord = filmsArr[Math.floor(Math.random() * filmsArr.length)];
   for (let i = 0; i < chosenWord.length; i++) {
     chosenWordArr.push("_");
   }
+  checkLetter();
+
+  button.classList.remove("hide");
+  alphabet.classList.remove("hide");
+  getLetter.classList.remove("hide");
+  lettersContainer.classList.remove("hide");
+  hangmanContainer.classList.remove("hide");
 };
 
+// ENTER A LETTER /////////////////////////////////////////////////////////////////
 // get the letter user has entered from input box
-const gamePlay = () => {
-  value = getLetter.value.toLowerCase();
-  checkPreviousLetters();
-  //   array to show user already chosen letters
-  checkScore();
-};
 
-//   check if the letter has already been used
-const checkPreviousLetters = () => {
-  for (let i = 0; i < usedLettersArr.length; i++) {
-    if (value === usedLettersArr[i]) {
-      errorMessage.innerHTML = `You've already tried the letter ${value}, please try another letter`;
-      gamePlay();
-    } else {
-      // addToUsedLetters();
-    }
+const checkScore = () => {
+  if (score === 0) {
+    gameOver();
+  } else if (!chosenWordArr.includes("_")) {
+    alert("you've won the game");
   }
 };
-
-//   loop over the chosen word, if the current letter is the same as what the user entered then loop over the array (currently represented by _ and replace with the correct letter)
+//   loop over the chosen word, if the letter is in the word then reveal it to user
+// (currently represented by _ and replace with the correct letter)
 const checkLetter = () => {
   for (let i = 0; i < chosenWord.length; i++) {
     if (chosenWord[i] === value) {
       chosenWordArr[i] = value;
-      score--;
+    } else {
     }
+  }
+  // if the array doesn't include the chosen letter then score ecreases by 1 and hangman is drawn
+  if (!chosenWordArr.includes(value)) {
+    score -= 1;
   }
   hiddenWord.innerHTML = chosenWordArr.join(" ");
 };
-
-const checkScore = () => {
-  if (score <= 0) {
-    gameOver();
-  } else {
-    checkLetter();
+const gamePlay = () => {
+  value = getLetter.value.toLowerCase();
+  checkLetter();
+  updateAlphabet();
+  updateScore();
+  checkScore();
+};
+const updateScore = () => {
+  if (score === 9) {
+    hangman[0].classList.remove("hide");
+  } else if (score === 8) {
+    hangman[1].classList.remove("hide");
+  } else if (score === 7) {
+    hangman[2].classList.remove("hide");
+  } else if (score === 6) {
+    hangman[3].classList.remove("hide");
+  } else if (score === 5) {
+    hangman[4].classList.remove("hide");
+  } else if (score === 4) {
+    hangman[5].classList.remove("hide");
+  } else if (score === 3) {
+    hangman[6].classList.remove("hide");
+  } else if (score === 2) {
+    hangman[7].classList.remove("hide");
+  } else if (score === 1) {
+    hangman[8].classList.remove("hide");
+  } else if (score === 0) {
+    hangman[9].classList.remove("hide");
   }
 };
-const gameOver = () => {
-  alert("game over");
+
+// remove the value from the alphabet, showing remaining letters to try
+const updateAlphabet = () => {
+  const index = alphabetArr.indexOf(value);
+  if (index > -1) {
+    alphabetArr.splice(index, 1);
+  }
+  alphabet.innerHTML = alphabetArr.join(" ");
 };
-start.addEventListener("click", chooseWord);
+
+const gameOver = () => {
+  gameOverMessage.classList.remove("hide");
+  restartGame.classList.remove("hide");
+};
+
+start.addEventListener("click", startGame);
 button.addEventListener("click", gamePlay);
 
-// show hangman animation
+// it's only updating the first letter in a sequence - managed to fix but need to find a away to update score, at the minute
+// it takes a point off if you get it right or not
