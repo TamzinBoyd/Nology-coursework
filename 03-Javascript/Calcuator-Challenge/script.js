c2289157af82b4f2b00a8ca92cd60de4ed661bae
Tamzin <tamzin_boyd@hotmail.co.uk>
2021-12-02 21:29:59 +0000

    improved functions (more pure)

--- 03-Javascript/Calcuator-Challenge/script.js
+++ 03-Javascript/Calcuator-Challenge/script.js
@@ -1,91 +1,68 @@
-// what do I want to happen
-// When user clicks on a number or operand it saves it & displays it
-// above needs to be limitless
-// When user clicks = the calculator needs to carry out the sum and display total
-// When user clicks AC display should reset back to 0
-
 // Ways to improve for future
 // currently the calcultor only accepts one operand
 
-//   Declare variables ////////////////////////////////////////////////////////////////////////////////
-//   empty array to push the values into
-let currentArray = [];
+let enteredValuesArr = [];
+let enteredOperand = "";
 
-//   display of sum and total saved in variables
 const display = document.getElementById("total");
 const equals = document.getElementById("equals");
-
-//   variable to layer take active operand from user's entry
-let finalOperand = "";
-
-// selects all number buttons then operand buttons
 const buttons = document.querySelectorAll(".number");
 const operands = document.querySelectorAll(".operand");
 
 // FUNCTIONS ///////////////////////////////////////////////////////////////////////////////////////////
 
 // adds the button's value to the display box on click
-function showNumber(event) {
-  currentArray.push(event.target.value);
-  display.innerHTML = currentArray.join("");
-}
+const displayUsersNum = (event, testArr) => {
+  enteredValuesArr.push(event.target.value);
+  display.innerHTML = enteredValuesArr.join("");
+};
 
-function showOperand(event) {
-  // adds selected operand to the display then joins it with a space
-  currentArray.push(event.target.value);
-  display.innerHTML = currentArray.join("");
+const displayUsersOperand = (event) => {
+  enteredValuesArr.push(event.target.value);
+  display.innerHTML = enteredValuesArr.join("");
 
-  // filters currentarray to take the value of the operand selected by user
-  finalOperand = currentArray.filter((currentArray) => {
-    // This returns the operand clicked by the user
-    return currentArray === event.target.value;
+  // filters values entered to take the value of the operand
+  enteredOperand = enteredValuesArr.filter((value) => {
+    //  returns the operand clicked by the user
+    return value === event.target.value;
   });
-  //   turns it into a string for later use
-  finalOperand = finalOperand.toString();
-}
+  enteredOperand = enteredOperand.toString();
+};
 
-// equals function to carry out final sum
-function calculateTotal(event) {
+const calculateTotal = (event) => {
   //  changes array to a new variable but joins it together
-  const calculateStr = currentArray.join("");
-  //   creates variable for the 2 separate parts & operand
-  const operandPos = calculateStr.indexOf(finalOperand);
-  //   creates substring of value upto finaloperand
-  const firstNum = calculateStr.substring(0, operandPos);
-  //   as above but takes second value
-  const secondNum = calculateStr.substring(operandPos + 1);
+  const combinedValues = enteredValuesArr.join("");
+  const getOperandPosition = combinedValues.indexOf(enteredOperand);
+  //   creates substring of value upto enteredOperand
+  const firstNum = combinedValues.substring(0, getOperandPosition);
+  const secondNum = combinedValues.substring(getOperandPosition + 1);
+
   //   carry out the sum depending on final operand value
-  if (finalOperand === "+") {
+  if (enteredOperand === "+") {
     display.innerHTML = parseFloat(firstNum) + parseFloat(secondNum);
-  } else if (finalOperand === "-") {
+  } else if (enteredOperand === "-") {
     display.innerHTML = parseFloat(firstNum) - parseFloat(secondNum);
-  } else if (finalOperand === "x") {
+  } else if (enteredOperand === "x") {
     display.innerHTML = parseFloat(firstNum) * parseFloat(secondNum);
-  } else if (finalOperand === "/") {
+  } else if (enteredOperand === "/") {
     display.innerHTML = parseFloat(firstNum) / parseFloat(secondNum);
   }
-}
+};
 
-// when user selects clear the display returns to 0 and array is reset to empty array
-function clear(event) {
+const clear = (event) => {
   display.innerHTML = "0.00";
+  enteredValuesArr = [];
+};
 
-  currentArray = [];
-}
+//  EVENT LISTENERS ////////////////////////////////////////////////
 
-//  EVENT LISTENERS ////////////////////////////////////////////////////
-// adds click to all buttons & carry out function which adds clicked values to the display
 for (var i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener("click", showNumber);
+  buttons[i].addEventListener("click", displayUsersNum);
 }
 
-// Same as above for operands
 for (var i = 0; i < operands.length; i++) {
-  operands[i].addEventListener("click", showOperand);
+  operands[i].addEventListener("click", displayUsersOperand);
 }
 
-// Equals button to carry out end sum
 equals.addEventListener("click", calculateTotal);
-
-// AC clear button
 document.querySelector(".clear").addEventListener("click", clear);
--- 03-Javascript/Football-Scores-Game/script.js
+++ 03-Javascript/Football-Scores-Game/script.js
@@ -1,41 +1,39 @@
-//  This lesson is about problem solving & sudo coding
-
-//  bigger problem - create an app to keep track of a score in a football match
-
-// What kind of application?
-// Scores for both teams?
-// what else is involved - players, time limit etc. What's in scope.
-
-// 2 teams in football so 2 scores are needed
+// create an app to keep track of a score in a football match
 
 let teamScore1 = 0;
 let teamScore2 = 0;
 let isGameStopped = false;
 
+const team1 = document.querySelector(".button__team1");
+const team2 = document.querySelector(".button__team2");
+
 // When team 1 button is clicked, increment team 1 score
-
-function handleTeam1Click() {
-  teamScore1 = teamScore1 + 1;
+const calculateTeam1Score = () => {
+  teamScore1++;
+  if (teamScore1 === 1) {
+    team1.innerHTML = `Team 1 has ${teamScore1} goal`;
+  } else if (teamScore1 > 1) {
+    team1.innerHTML = `Team 1 has ${teamScore1} goals`;
+  }
   alert("The score for team one is now " + teamScore1);
-}
+};
 
-// Same for team 2
-
-function handleTeam2Click() {
-  teamScore2 = teamScore2 + 1;
+const handleTeam2Click = () => {
+  teamScore2++;
+  if (teamScore2 === 1) {
+    team2.innerHTML = `Team 2 has ${teamScore2} goal`;
+  } else if (teamScore2 > 1) {
+    team2.innerHTML = `Team 2 has ${teamScore2} goals`;
+  }
   alert("The score for team two is now " + teamScore2);
-}
+};
 
-// when do we stop? Timer or another button?
-
-function handleStopGame() {
+const handleStopGame = () => {
   isGameStopped = true;
   alert(
-    "The game has ended, team one have " +
-      teamScore1 +
-      " goals, and team two have " +
-      teamScore2 +
-      " goals"
+    `The game has ended, team one have ${teamScore1}
+       goals, and team two have ${teamScore2}
+       goals`
   );
   if (teamScore1 > teamScore2) {
     alert("Team one win the game");
@@ -44,4 +42,6 @@ function handleStopGame() {
   } else {
     alert("It's a draw");
   }
-}
+};
+
+team1.addEventListener("click", calculateTeam1Score);
