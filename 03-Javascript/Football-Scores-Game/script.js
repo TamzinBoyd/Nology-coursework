778ce1526acb09b15a3da9419be59402360e15ec
Tamzin <tamzin_boyd@hotmail.co.uk>
2021-12-05 21:00:46 +0000

    React Module added

--- 03-Javascript/Football-Scores-Game/script.js
+++ 03-Javascript/Football-Scores-Game/script.js
@@ -6,42 +6,64 @@ let isGameStopped = false;
 
 const team1 = document.querySelector(".button__team1");
 const team2 = document.querySelector(".button__team2");
+const endGame = document.querySelector(".endGameButton");
 
 // When team 1 button is clicked, increment team 1 score
-const calculateTeam1Score = () => {
-  teamScore1++;
-  if (teamScore1 === 1) {
-    team1.innerHTML = `Team 1 has ${teamScore1} goal`;
-  } else if (teamScore1 > 1) {
-    team1.innerHTML = `Team 1 has ${teamScore1} goals`;
+const calculateTeam1Score = (score) => {
+  score++;
+  if (score === 1) {
+    team1.innerHTML = `Team 1 has ${score} goal`;
+  } else if (score > 1) {
+    team1.innerHTML = `Team 1 has ${score} goals`;
   }
-  alert("The score for team one is now " + teamScore1);
+  alert("The score for team one is now " + score);
 };
 
-const handleTeam2Click = () => {
-  teamScore2++;
-  if (teamScore2 === 1) {
-    team2.innerHTML = `Team 2 has ${teamScore2} goal`;
-  } else if (teamScore2 > 1) {
-    team2.innerHTML = `Team 2 has ${teamScore2} goals`;
+const calculateTeam2Score = (score) => {
+  score++;
+  if (score === 1) {
+    team2.innerHTML = `Team 2 has ${score} goal`;
+  } else if (score > 1) {
+    team2.innerHTML = `Team 2 has ${score} goals`;
   }
-  alert("The score for team two is now " + teamScore2);
+  alert("The score for team two is now " + score);
 };
 
-const handleStopGame = () => {
+const handleStopGame = (score1, score2) => {
   isGameStopped = true;
   alert(
-    `The game has ended, team one have ${teamScore1}
-       goals, and team two have ${teamScore2}
+    `The game has ended, team one have ${score1}
+       goals, and team two have ${score2}
        goals`
   );
-  if (teamScore1 > teamScore2) {
+  if (score1 > score2) {
     alert("Team one win the game");
-  } else if (teamScore2 > teamScore1) {
+  } else if (score2 > score1) {
     alert("Team two win the game");
   } else {
     alert("It's a draw");
   }
+  team1.innerHTML = "Team 1 Scores";
+  team2.innerHTML = "Team 2 Scores";
 };
 
-team1.addEventListener("click", calculateTeam1Score);
+team1.addEventListener(
+  "click",
+  (getScore1 = () => {
+    calculateTeam1Score(teamScore1);
+  })
+);
+
+team2.addEventListener(
+  "click",
+  (getScore2 = () => {
+    calculateTeam2Score(teamScore2);
+  })
+);
+
+endGame.addEventListener(
+  "click",
+  (getScore2 = () => {
+    handleStopGame(teamScore1, teamScore2);
+  })
+);
