6d359ead5b97e7f9ff7d6be6ee10982847576c38
TamzinBoyd <tamzin_boyd@hotmail.co.uk>
2021-12-18 10:41:45 +0000

    football game updated

--- 03-Javascript/Football-Scores-Game/script.js
+++ 03-Javascript/Football-Scores-Game/script.js
@@ -4,17 +4,17 @@ let teamScore1 = 0;
 let teamScore2 = 0;
 let isGameStopped = false;
 
-const team1 = document.querySelector(".button__team1");
-const team2 = document.querySelector(".button__team2");
+const team1HTML = document.querySelector(".button__team1");
+const team2HTML = document.querySelector(".button__team2");
 const endGame = document.querySelector(".endGameButton");
 
 // When team 1 button is clicked, increment team 1 score
-const calculateTeam1Score = (score) => {
+const calculateteam1Score = (score) => {
   score++;
   if (score === 1) {
-    team1.innerHTML = `Team 1 has ${score} goal`;
+    team1HTML.innerHTML = `Team 1 has ${score} goal`;
   } else if (score > 1) {
-    team1.innerHTML = `Team 1 has ${score} goals`;
+    team1HTML.innerHTML = `Team 1 has ${score} goals`;
   }
   alert("The score for team one is now " + score);
 };
@@ -22,20 +22,16 @@ const calculateTeam1Score = (score) => {
 const calculateTeam2Score = (score) => {
   score++;
   if (score === 1) {
-    team2.innerHTML = `Team 2 has ${score} goal`;
+    team2HTML.innerHTML = `Team 2 has ${score} goal`;
   } else if (score > 1) {
-    team2.innerHTML = `Team 2 has ${score} goals`;
+    team2HTML.innerHTML = `Team 2 has ${score} goals`;
   }
   alert("The score for team two is now " + score);
 };
 
+// show final goals
 const handleStopGame = (score1, score2) => {
   isGameStopped = true;
-  alert(
-    `The game has ended, team one have ${score1}
-       goals, and team two have ${score2}
-       goals`
-  );
   if (score1 > score2) {
     alert("Team one win the game");
   } else if (score2 > score1) {
@@ -43,18 +39,19 @@ const handleStopGame = (score1, score2) => {
   } else {
     alert("It's a draw");
   }
-  team1.innerHTML = "Team 1 Scores";
-  team2.innerHTML = "Team 2 Scores";
+  team1HTML.innerHTML = "Team 1 Scores";
+  team2HTML.innerHTML = "Team 2 Scores";
 };
 
-team1.addEventListener(
+// click events
+team1HTML.addEventListener(
   "click",
   (getScore1 = () => {
     calculateTeam1Score(teamScore1);
   })
 );
 
-team2.addEventListener(
+team2HTML.addEventListener(
   "click",
   (getScore2 = () => {
     calculateTeam2Score(teamScore2);
